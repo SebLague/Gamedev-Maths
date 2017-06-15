20a26369670ce93fbca34293f1f05ce5d25caa4a
Sebastian <seblague@yahoo.com>
2017-06-15 13:17:44 +0200

    Create AllGeometry.cs

--- AllGeometry.cs
+++ AllGeometry.cs
@@ -27,14 +27,14 @@ public static class Geometry
 	}
 
 	/// Determines whether point P is inside the triangle ABC
-	public static bool PointInTriangle(Point A, Point B, Point C, Point P)
+	public static bool PointInTriangle(Point a, Point b, Point c, Point p)
 	{
-		double s1 = C.y - A.y;
-		double s2 = C.x - A.x;
-		double s3 = B.y - A.y;
-		double s4 = P.y - A.y;
+		double s1 = c.y - a.y;
+		double s2 = c.x - a.x;
+		double s3 = b.y - a.y;
+		double s4 = p.y - a.y;
 
-		double w1 = (A.x * s1 + s4 * s2 - P.x * s1) / (s3 * s2 - (B.x-A.x) * s1);
+		double w1 = (a.x * s1 + s4 * s2 - p.x * s1) / (s3 * s2 - (b.x-a.x) * s1);
 		double w2 = (s4- w1 * s3) / s1;
 		return w1 >= 0 && w2 >= 0 && (w1 + w2) <= 1;
 	}
